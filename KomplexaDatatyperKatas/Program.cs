c1f41b1c283d4b0dfb743b8b5978ea68d8ec4406
Björn Strömberg <bjorn.stromberg@codic.se>
2021-08-31 23:36:39 +0200

    Ny kata för kommande föreläsning

---
+++ KomplexaDatatyperKatas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KomplexaDatatyperKatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1: Order an electric bicycle");
+                Console.WriteLine("2: Order a trampoline");
+                Console.WriteLine("3: Order a bouquet");
+                Console.WriteLine("4: Order something else");
+                Console.WriteLine("5: Show all orders");
+                Console.WriteLine("6: Show amount of each order");
+                Console.WriteLine("7: Exit");
+
+                Console.Write("Type option and press enter:");
+                int choice = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                if (choice == 1)
+                {
+
+                }
+                else if (choice == 2)
+                {
+
+                }
+                else if(choice == 7)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
